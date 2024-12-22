0428eba75f9969799a816695c49204fcafc7b7ba
sujaysharvesh <119726302+sujaysharvesh@users.noreply.github.com>
2024-12-22 15:18:28 +0530

    Update middleware.py

--- app/middleware.py
+++ app/middleware.py
@@ -34,5 +34,5 @@ def register_middleware(app: FastAPI):
 
     app.add_middleware(
         TrustedHostMiddleware,
-        allowed_hosts=["localhost", "127.0.0.1" ,"bookstar-uxiv.onrender.com","0.0.0.0"],
+        allowed_hosts=["localhost", "127.0.0.1" ,"bookstar-uxiv.onrender.com"],
     )
